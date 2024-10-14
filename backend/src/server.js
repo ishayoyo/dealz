4f2a1eb6626426ece65f313a9e877d16c0f2835f
ishayoyo <ishay.almuly@gmail.com>
2024-10-15 00:54:29 +0300

    deals delete old server backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ console.log('NODE_ENV:', process.env.NODE_ENV);
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ["https://saversonic.com", "https://deals.ishay.me", "http://localhost:3000"],
+    origin: ["https://saversonic.com", "http://localhost:3000"],
     methods: ["GET", "POST"],
     credentials: true
   }
