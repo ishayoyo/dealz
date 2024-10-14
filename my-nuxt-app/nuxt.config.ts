a14d4083699d8a1bfe1112e8841438d4ebfb6844
ishayoyo <ishay.almuly@gmail.com>
2024-10-14 21:56:03 +0300

    favicons

--- my-nuxt-app/nuxt.config.ts
+++ my-nuxt-app/nuxt.config.ts
@@ -39,6 +39,15 @@ export default defineNuxtConfig({
 
   // Add this new section for error handling
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  }
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+    },
+  },
 })
