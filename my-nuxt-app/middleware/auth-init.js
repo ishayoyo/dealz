45b6afe04974638cfa7837d3642f6b350f1ee302
ishayoyo <ishay.almuly@gmail.com>
2024-11-01 00:42:19 +0200

    fix auth partially

--- my-nuxt-app/middleware/auth-init.js
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (process.client) {
-    const authStore = useAuthStore();
-    if (!authStore.isInitialized) {
-      await authStore.initializeAuth();
-    }
-  }
-});
