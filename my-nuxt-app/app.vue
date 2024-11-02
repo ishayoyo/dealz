9aef38c6313d5ab86a1cac3123a66f8afcb51736
ishayoyo <ishay.almuly@gmail.com>
2024-11-02 20:42:16 +0200

    fix load state

--- my-nuxt-app/app.vue
+++ my-nuxt-app/app.vue
@@ -1,34 +1,22 @@
 <template>
   <div class="app-wrapper">
-    <Transition name="fade-slow">
-      <SiteLoader v-if="isLoading" />
-    </Transition>
-    
-    <Transition name="fade-slow">
-      <div v-show="!isLoading" class="app-content">
-        <NuxtLayout>
-          <NuxtPage />
-        </NuxtLayout>
-      </div>
-    </Transition>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useAuthStore } from '~/stores/auth'
 
-const isLoading = ref(true)
 const authStore = useAuthStore()
 
 onMounted(async () => {
   try {
     await authStore.initializeAuth()
-    await new Promise(resolve => setTimeout(resolve, 1500))
   } catch (error) {
     console.error('Initial load error:', error)
-  } finally {
-    isLoading.value = false
   }
 })
 </script>
